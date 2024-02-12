4d2973cb712009a869e32470fc916ab732da8824
isha-mistry <ishamistry.lamprostech@gmail.com>
2024-02-12 16:00:02 +0530

    Changes in the styles of notification bar

--- src/components/DAOs/ExploreDAOs.tsx
+++ src/components/DAOs/ExploreDAOs.tsx
@@ -43,13 +43,69 @@ function ExploreDAOs() {
 
   return (
     <div className="p-6">
+      <div className="">
+        <div className="text-blue-shade-200 font-medium text-4xl font-quanty pb-4">
+          Explore DAOs
+        </div>
+
+        <div
+          style={{ background: "rgba(238, 237, 237, 0.36)" }}
+          className="flex border-[0.5px] border-black w-fit rounded-full my-3 font-poppins"
+        >
+          <input
+            type="text"
+            placeholder="Search DAOs"
+            style={{ background: "rgba(238, 237, 237, 0.36)" }}
+            className="pl-5 rounded-full outline-none"
+            value={searchQuery}
+            onChange={(e) => handleSearchChange(e.target.value)}
+          ></input>
+          <span className="flex items-center bg-black rounded-full px-6 py-3">
+            <Image src={search} alt="search" width={20} />
+          </span>
+        </div>
+
+        <div className="grid min-[475px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-10 py-8 font-poppins">
+          {daoInfo.length > 0 ? (
+            daoInfo.map((daos, index) => (
+              <div
+                key={daos.name}
+                style={{ boxShadow: "0px 4px 50.8px 0px rgba(0, 0, 0, 0.11)" }}
+                className="px-5 py-7 rounded-2xl cursor-pointer"
+                onClick={() => handleClick(daos.name, daos.img)}
+              >
+                <div className="flex justify-center">
+                  <Image
+                    src={daos.img}
+                    alt="Image not found"
+                    width={80}
+                    className="rounded-full"
+                  ></Image>
+                </div>
+                <div className="text-center">
+                  <div className="py-3">
+                    <div className="font-semibold">{daos.name}</div>
+                    <div className="text-sm bg-[#F2F2F2] py-2 rounded-md mt-3">
+                      {daos.value} Participants
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <div className="pl-3 text-xl font-semibold">
+              No such Dao available
+            </div>
+          )}
+        </div>
+      </div>
+
       <div
-        style={{ boxShadow: "0px 4px 50.8px 0px rgba(0, 0, 0, 0.11)" }}
-        className={`flex absolute items-center justify-center z-30 bottom-5  rounded-full px-6 font-poppins text-sm font-medium ${
+        className={`flex absolute items-center justify-center top-4 rounded-full font-poppins text-sm font-medium left-[34%] w-[32rem] ${
           status ? "" : "hidden"
         }`}
       >
-        <div className="py-3">
+        <div className="py-2 bg-blue-shade-100 text-white rounded-full px-7">
           To ensure optimal user experience, please note that our site is
           designed to be responsive on desktop devices.
         </div>
@@ -59,60 +115,6 @@ function ExploreDAOs() {
         >
           <ImCross color="#fff" size={10} />
         </div>
-      </div>
-      <div className="text-blue-shade-200 font-medium text-4xl font-quanty pb-4">
-        Explore DAOs
-      </div>
-
-      <div
-        style={{ background: "rgba(238, 237, 237, 0.36)" }}
-        className="flex border-[0.5px] border-black w-fit rounded-full my-3 font-poppins"
-      >
-        <input
-          type="text"
-          placeholder="Search DAOs"
-          style={{ background: "rgba(238, 237, 237, 0.36)" }}
-          className="pl-5 rounded-full outline-none"
-          value={searchQuery}
-          onChange={(e) => handleSearchChange(e.target.value)}
-        ></input>
-        <span className="flex items-center bg-black rounded-full px-6 py-3">
-          <Image src={search} alt="search" width={20} />
-        </span>
-      </div>
-
-      <div className="grid min-[475px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-10 py-8 font-poppins">
-        {daoInfo.length > 0 ? (
-          daoInfo.map((daos, index) => (
-            <div
-              key={daos.name}
-              style={{ boxShadow: "0px 4px 50.8px 0px rgba(0, 0, 0, 0.11)" }}
-              className="px-5 py-7 rounded-2xl cursor-pointer"
-              onClick={() => handleClick(daos.name, daos.img)}
-            >
-              <div className="flex justify-center">
-                <Image
-                  src={daos.img}
-                  alt="Image not found"
-                  width={80}
-                  className="rounded-full"
-                ></Image>
-              </div>
-              <div className="text-center">
-                <div className="py-3">
-                  <div className="font-semibold">{daos.name}</div>
-                  <div className="text-sm bg-[#F2F2F2] py-2 rounded-md mt-3">
-                    {daos.value} Participants
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))
-        ) : (
-          <div className="pl-3 text-xl font-semibold">
-            No such Dao available
-          </div>
-        )}
       </div>
     </div>
   );
