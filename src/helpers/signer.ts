9edfec6b8f69f4292b4743e3d7f078a478f03207
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2024-02-15 18:06:18 +0530

    added Attestation APIs and security features

--- src/helpers/signer.ts
+++ src/helpers/signer.ts
@@ -2,12 +2,31 @@ import { createWalletClient, createPublicClient, custom } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { optimism } from "viem/chains";
 
-export const publicClient = createPublicClient({
-  chain: optimism,
-  transport: custom(window.ethereum!),
-});
+declare global {
+  interface Window {
+    ethereum?: any;
+    // Add other properties if needed
+  }
+}
 
-export const walletClient = createWalletClient({
-  chain: optimism,
-  transport: custom(window.ethereum!),
-});
+let publicClient;
+let walletClient;
+
+if (typeof window !== "undefined" && window.ethereum) {
+  // Instantiate public client and wallet client
+  publicClient = createPublicClient({
+    chain: optimism,
+    transport: custom(window.ethereum),
+  });
+
+  walletClient = createWalletClient({
+    chain: optimism,
+    transport: custom(window.ethereum),
+  });
+
+  // Now you can use publicClient and walletClient as needed
+} else {
+  console.error("window.ethereum is not available");
+}
+
+export { publicClient, walletClient };
