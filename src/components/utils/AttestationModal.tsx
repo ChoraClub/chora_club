608a953dcccb10b4870b452088a608193a96f136
isha-mistry <ishamistry.lamprostech@gmail.com>
2024-03-08 11:46:46 +0530

    Attestation modal added

--- src/components/utils/AttestationModal.tsx
+++ src/components/utils/AttestationModal.tsx
@@ -1,54 +1,61 @@
-import React from "react";
-import {
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Button,
-  useDisclosure,
-} from "@nextui-org/react";
-import img from "@/assets/images/daos/Rectangle 362.png";
+import React, { useState } from "react";
+import img from "@/assets/images/daos/attestation.png";
 import Image from "next/image";
+import { RxCross2 } from "react-icons/rx";
 
 function AttestationModal() {
-  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModalOpen(!modalOpen);
+  };
 
   return (
     <div>
       <button
         className="bg-blue-shade-200 font-bold text-white rounded-full px-8 py-[10px]"
-        onClick={onOpen}
+        onClick={toggleModal}
       >
         Modal
       </button>
-      <Modal
-        isOpen={isOpen}
-        onOpenChange={onOpenChange}
-        className="font-poppins p-0"
-        size="5xl"
-      >
-        <ModalContent>
-          {(onClose: any) => (
-            <>
-              <ModalBody>
-                <div className="flex">
-                  <Image src={img} alt="image" />
-                  <div className="flex flex-col items-center justify-center w-auto">
-                    <div>Congratulations</div>
-                    <button className="bg-blue-shade-100 rounded-full text-white px-5 py-2 font-medium">
-                      Attest Onchain
-                    </button>
-                    <button className="bg-blue-shade-100 rounded-full text-white px-5 py-2 font-medium">
-                      Attest Offchain
-                    </button>
-                  </div>
+      {modalOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+          <div
+            className="absolute inset-0 bg-gray-800 opacity-50"
+            onClick={toggleModal}
+          ></div>
+          <div className="z-50 bg-white rounded-3xl max-w-7xl">
+            <div className="flex justify-evenly">
+              <div>
+                <Image
+                  src={img}
+                  alt="image"
+                  height={300}
+                  width={300}
+                  className="rounded-3xl"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center relative px-20">
+                <button
+                  className="text-gray-500 hover:text-gray-800 absolute top-3 right-4"
+                  onClick={toggleModal}
+                >
+                  <RxCross2 size={20} />
+                </button>
+                <h2 className="text-2xl font-bold">Claim Attestations! 🎉</h2>
+                <div className="justify-around space-x-8 py-5">
+                  <button className="border-2 border-blue-shade-200 bg-blue-shade-200 rounded-full text-white px-8 py-3 font-bold text-sm">
+                    On-chain
+                  </button>
+                  <button className="border-2 border-blue-shade-200 rounded-full text-blue-shade-200 px-8 py-3 font-bold text-sm">
+                    Off-chain
+                  </button>
                 </div>
-              </ModalBody>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
