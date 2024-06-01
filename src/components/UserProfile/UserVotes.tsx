bb07a8019103b00ab8b4c72a2381a6e4da859724
Vinit5994 <vinitpithadiya07@gmail.com>
2024-06-01 13:23:50 +0530

    proposals of deletage from subgraph

--- src/components/UserProfile/UserVotes.tsx
+++ src/components/UserProfile/UserVotes.tsx
@@ -7,10 +7,13 @@ import styles from "../IndividualDelegate/DelegateVotes.module.css";
 import { Oval } from "react-loader-spinner";
 import { useAccount } from "wagmi";
 import { useNetwork } from "wagmi";
+import VotedOnOptions from "@/assets/images/votedOnOption.png";
+import Image from "next/image";
+import { Tooltip as NextUITooltip } from "@nextui-org/react";
 Chart.register(ArcElement, Tooltip, Legend);
 
 function UserVotes({ daoName }: { daoName: string }) {
-  const { address } = useAccount();
+  const { address } = useAccount(); 
   const { chain, chains } = useNetwork();
   const [first, setFirst] = useState<boolean>(false);
   const [currentPage, setCurrentPage] = useState<number>(1);
@@ -21,6 +24,7 @@ function UserVotes({ daoName }: { daoName: string }) {
   const [supportCounts, setSupportCounts] = useState({ 0: 0, 1: 0, 2: 0 });
   const [clientURL, setClientURL] = useState<string>("");
   const [client, setClient] = useState<any>(null);
+  const [dataToShow, setDataToShow] = useState<any>([]);
 
   useEffect(() => {
     setIsPageLoading(false);
@@ -29,27 +33,30 @@ function UserVotes({ daoName }: { daoName: string }) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        setIsPageLoading(true);
-        const myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
+        if (daoName === "arbitrum") {
+          setIsPageLoading(true);
+          const myHeaders = new Headers();
+          myHeaders.append("Content-Type", "application/json");
 
-        const requestOptions: RequestInit = {
-          method: "GET",
-          headers: myHeaders,
-        };
+          const requestOptions: RequestInit = {
+            method: "GET",
+            headers: myHeaders,
+          };
 
-        const response = await fetch(
-          `/api/get-dao-details/${daoName}`,
-          requestOptions
-        );
-        const result = await response.json();
+          const response = await fetch(
+            `/api/get-dao-details/arbitrum`,
+            requestOptions
+          );
+          const result = await response.json();
 
-        console.log(
-          "dao-details",
-          result.data[0].api_links.subgraph.past_votes
-        );
-        setClientURL(result.data[0].api_links.subgraph.past_votes);
-        setIsPageLoading(false);
+          console.log(
+            "dao-details",
+            result.data[0].api_links.subgraph.past_votes
+          );
+          setClientURL(result.data[0].api_links.subgraph.past_votes);
+        
+          setIsPageLoading(false);
+        }
       } catch (error) {
         console.error(error);
         setIsPageLoading(false);
@@ -69,9 +76,120 @@ function UserVotes({ daoName }: { daoName: string }) {
     }
   }, [clientURL]);
 
+  const op_client = createClient({
+    url: "https://api.studio.thegraph.com/query/68573/v6_proxy/version/latest",
+    exchanges: [cacheExchange, fetchExchange],
+  });
+
+
+  const opQuery = (first: any, skip: any) => gql`
+  query MyQuery($address: String!) {
+    voteCasts(
+      where: { voter: $address}
+      orderDirection: desc
+      orderBy: blockTimestamp
+      first: ${first}
+      skip: ${skip}
+    ) {
+      proposalId
+      reason
+      support
+      weight
+      transactionHash
+      blockTimestamp
+    }
+  
+    voteCastWithParams_collection(
+      where: {voter: $address}
+      orderBy: blockTimestamp
+      orderDirection: desc
+    ) {
+      proposalId
+      reason
+      support
+      weight
+      transactionHash
+      blockTimestamp
+      params
+    }
+  }
+  `;
+  const opDescription = gql`
+  query MyDescriptionQuery($proposalId: String!) {
+    proposalCreated1S(where: { proposalId: $proposalId }) {
+      description
+    }
+    proposalCreated2S(where: { proposalId: $proposalId }) {
+      description
+    }
+    proposalCreated3S(where: { proposalId: $proposalId }) {
+      description
+    }
+    proposalCreateds(where: { proposalId: $proposalId }) {
+      description
+    }
+  }
+`;
+
+  const fetchProposalDescriptions = async (first = 100, skip = 0) => {
+    if (daoName === "optimism") {
+      const proposalIdsResult: any = await op_client.query(opQuery(first, skip), { address: address });
+      // console.log("result", proposalIdsResult);
+
+      const voteCasts = proposalIdsResult.data.voteCasts || [];
+      const voteCastWithParamsCollection = proposalIdsResult.data.voteCastWithParams_collection || [];
+
+      // Combine the data
+      const combinedData = [...voteCasts, ...voteCastWithParamsCollection];
+
+      // Sort combined data by blockTimestamp in descending order
+      combinedData.sort((a: any, b: any) => b.blockTimestamp - a.blockTimestamp);
+      // console.log(combinedData)
+      const proposalIds = combinedData.map((voteCast: any) => voteCast);
+
+
+      // console.log("Id", proposalIds);
+
+      const descriptionsPromises = proposalIds.map((proposalId: any) => {
+        // console.log("proposalId...", proposalId);
+        return op_client.query(opDescription, { proposalId: proposalId.proposalId.toString() }).toPromise();
+      });
+
+      // console.log("descriptionsPromises", descriptionsPromises);
+
+      const descriptionsResults = await Promise.all(descriptionsPromises);
+      // console.log("descriptionsResults", descriptionsResults);
+      const FinalResult = descriptionsResults
+        .flatMap((result: any, index: any) =>
+          Object.values(result.data)
+            .flat()
+            .filter((d: any) => d.description)
+            .map((d: any) => ({
+              proposalId: proposalIds[index],
+              proposal: { description: d.description, },
+              support: proposalIds[index].support
+            }))
+        )
+        .filter((item: any) => item.proposal.description.length > 0);
+
+      // console.log("FinalResult", FinalResult);
+      setPageData(FinalResult);
+      setIsPageLoading(false);
+      setGraphData(FinalResult);
+      setFirst(true);
+
+      // return FinalResult;
+    }
+  };
   useEffect(() => {
-    const fetchGraphData = async () => {
-      if (client && daoName) {
+    fetchProposalDescriptions();
+  }, [daoName]);
+  ;
+
+
+  useEffect(() => {
+    const fetchGraphDataAr = async () => {
+      if (client && daoName === "arbitrum") {
         try {
           setIsPageLoading(true);
           const query = gql`
@@ -79,7 +197,7 @@ function UserVotes({ daoName }: { daoName: string }) {
               votes(
                 orderBy: timestamp
                 orderDirection: desc
-                where: { user: $address, organization: "${daoName}.eth" }
+                where: { user: $address, organization: "arbitrum.eth" }
               ) {
                 id
                 proposal {
@@ -100,32 +218,55 @@ function UserVotes({ daoName }: { daoName: string }) {
           const gqdata: any = await client.query(query, {
             address: address,
           });
-          console.log("Urql: ", gqdata.data.votes);
-          setGraphData(gqdata.data.votes);
-
-          const op_counts = gqdata.data.votes.reduce(
-            (acc: any, curr: any) => {
-              const support = curr.support;
-              acc[support] = (acc[support] || 0) + 1;
-              return acc;
-            },
-            { 0: 0, 1: 0, 2: 0 }
-          );
-          setSupportCounts(op_counts);
+          // console.log("url",clientURL)
+          // console.log("Urql: ", gqdata.data.votes);
+          const fetchedVotes = gqdata.data.votes;
+          setGraphData(fetchedVotes);
+          setPageData(fetchedVotes);
+          // console.log("Graph Data: ", graphData);
+         
+          // console.log("Page Data: ", pageData);
           setFirst(true);
           setIsPageLoading(false);
         } catch (error) {
           console.error(error);
           setIsPageLoading(false);
         }
-      } else {
-        setGraphData([]);
-        setFirst(false);
       }
     };
 
-    fetchGraphData();
+    fetchGraphDataAr();
   }, [client, daoName, address]);
+
+  
+  useEffect(() => {
+
+    const fetchGraphData = async () => {
+      // if (daoName == "optimism") {
+        const op_counts = pageData.reduce(
+          (acc: any, curr: any) => {
+            // console.log("curr", curr.proposalId.params.length)
+            const support = (curr.proposalId && curr.proposalId.params && curr.proposalId.params.length > 2) ? 1 : curr.support;
+            // console.log(support)
+            acc[support] = (acc[support] || 0) + 1;
+            return acc;
+          },
+          { 0: 0, 1: 0, 2: 0, 3: 0 }
+        );
+        // console.log("op_counts", op_counts)
+        // console.log("pageData", pageData)
+        // setGraphData(pageData);
+        setSupportCounts(op_counts);
+        // setFirst(true);
+      // } else {
+
+      //   // setGraphData([]);
+      //   setFirst(false);
+      // }
+    };
+
+    fetchGraphData();
+  }, [pageData]);
 
   const totalData: number = graphData.length;
   const dataPerPage: number = 5;
@@ -133,11 +274,13 @@ function UserVotes({ daoName }: { daoName: string }) {
 
   useEffect(() => {
     const fetchPageData = async () => {
+
       const offset = (currentPage - 1) * dataPerPage;
       const end = offset + dataPerPage;
       const initialData = await graphData.slice(offset, end);
       // console.log("initial data: ", initialData);
-      setPageData(initialData);
+      setDataToShow(initialData);
+      console.log("data to show: ", dataToShow);
       // if (initialData) {
       setIsPageLoading(false);
       // }
@@ -149,7 +292,7 @@ function UserVotes({ daoName }: { daoName: string }) {
   }, [currentPage, graphData]);
 
   const chartData = {
-    labels: ["For", "Against", "Other"],
+    labels: [`For: ${supportCounts[1]} votes`, `Against: ${supportCounts[0]} votes`, `Absatin: ${supportCounts[2]} votes`],
     datasets: [
       {
         label: "# of Votes",
@@ -159,7 +302,18 @@ function UserVotes({ daoName }: { daoName: string }) {
       },
     ],
   };
+  function formatNumber(num: any) {
 
+    if (num >= 1e9) {
+      return (num / 1e9).toFixed(2) + 'B'; // Billion
+    } else if (num >= 1e6) {
+      return (num / 1e6).toFixed(2) + 'M'; // Million
+    } else if (num >= 1e3) {
+      return (num / 1e3).toFixed(2) + 'k'; // Thousand
+    } else {
+      return num.toFixed(1); // Less than a thousand
+    }
+  }
   return (
     <div className="pt-4">
       <div className="grid grid-cols-5 pe-5 gap-4 pb-6">
@@ -214,12 +368,12 @@ function UserVotes({ daoName }: { daoName: string }) {
                 />
               </div>
             ) : first && !isPageLoading && pageData.length > 0 ? (
-              pageData.map((proposal: any, index: number) => (
+              dataToShow.map((proposal: any, index: number) => (
                 <div
                   key={index}
                   className={`flex justify-between border border-[#7C7C7C] text-sm px-3 py-2 rounded-lg items-center my-3 `}
                 >
-                  <div className="w-4/5 break-words">
+                  <div className="w-3/4 break-words">
                     <div className={`${openDesc[index] ? "" : styles.desc}`}>
                       {proposal.proposal.description}
                     </div>
@@ -235,20 +389,51 @@ function UserVotes({ daoName }: { daoName: string }) {
                     </span>
                   </div>
                   <div
-                    className={`text-white rounded-full px-3 py-[2px] ${
-                      proposal.support === 1
-                        ? "bg-[#0033A8]"
-                        : proposal.support === 0
-                        ? "bg-[#6B98FF]"
-                        : "bg-[#004DFF]"
-                    }`}
+                    className={`text-white rounded-full px-2 py-[2px] w-[70px] me-1 text-center flex justify-center align-center
+                    ${daoName ==="optimism" && proposal.proposalId && proposal.proposalId.params && proposal.proposalId.params.length > 2
+                        ? "" // You can use a different background color for this condition
+                        : 
+                        proposal.support === 1
+                          ? "bg-[#0033A8]"
+                          : proposal.support === 0
+                            ? "bg-[#6B98FF]"
+                            : "bg-[#004DFF]"
+                      }`}
                   >
-                    {proposal.support === 1
-                      ? "For"
-                      : proposal.support === 0
-                      ? "Against"
-                      : "Other"}
+                    <NextUITooltip
+                      content="Voted on options"
+                      isDisabled={!(proposal.proposalId && proposal.proposalId.params && proposal.proposalId.params.length > 2)}
+                    >
+                      {
+                       proposal.proposalId&& proposal.proposalId.params && proposal.proposalId.params.length > 2
+                          ? <Image className="flex justify-center items-center" src={VotedOnOptions} alt="Voted on options" />
+                          : proposal.support === 1
+                            ? "For"
+                            : proposal.support === 0
+                              ? "Against"
+                              : "Abstain"
+                      }
+                    </NextUITooltip>
                   </div>
+                  <NextUITooltip content="Votes">
+                    {daoName === "optimism" && proposal.proposalId && (
+                      <div className="text-white rounded-full px-3 py-[2px] bg-[#FF0000]">
+                        {formatNumber(proposal.proposalId.weight / 1e18)}
+                      </div>
+                    )}
+                  </NextUITooltip>
+
+                  {daoName === "optimism"&& proposal.proposalId && (
+                    <div className="px-3 py-[2px] text-xl">
+                      <a
+                        href={`https://optimistic.etherscan.io/tx/${proposal.proposalId.transactionHash}`}
+                        target="_blank"
+                        className="cursor-pointer"
+                      >
+                        ↗
+                      </a>
+                    </div>
+                  )}
                 </div>
               ))
             ) : (
@@ -263,9 +448,8 @@ function UserVotes({ daoName }: { daoName: string }) {
             ""
           ) : (
             <div
-              className={`pt-4 flex items-end bottom-0 justify-center ${
-                graphData.length == 0 ? "hidden" : ""
-              }`}
+              className={`pt-4 flex items-end bottom-0 justify-center ${graphData.length == 0 ? "hidden" : ""
+                }`}
             >
               <Pagination
                 color="primary"
