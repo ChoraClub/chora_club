c21c0f573e637c4b139d325d1419a2c52402d384
purvik6062 <purvikpanchal6336@gmail.com>
2024-06-07 16:23:53 +0530

    change in constant file

--- src/config/constants.tsx
+++ src/config/constants.tsx
@@ -1,5 +1,5 @@
 export const DB_NAME =
-  process.env.NODE_ENV == "development" ? "chora-club" : "chora-club";
+  process.env.NODE_ENV == "development" ? "chora-club-dev" : "chora-club";
 
 export const BASE_URL =
   process.env.NODE_ENV == "development"
