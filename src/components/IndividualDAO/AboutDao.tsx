05515fd7e6f9f2ebfb4433c9635072e836b157ff
Vinit5994 <vinitpithadiya07@gmail.com>
2024-06-29 19:01:04 +0530

    added json file

--- src/components/IndividualDAO/AboutDao.tsx
+++ src/components/IndividualDAO/AboutDao.tsx
@@ -13,80 +13,54 @@ import {
   FaMediumM,
   FaTwitch,
 } from "react-icons/fa";
-import { IoEarth } from "react-icons/io5";
+import about from "../../utils/about_dao.json";
 
-function AboutDao({ props }: { props: string }) {
+const processText = (text:any) => {
+  // Replace headings
+  text = text.replace(/^# (.+)$/gm, '<h1>$1</h1>');
+  text = text.replace(/^## (.+)$/gm, '<h2>$1</h2>');
+  text = text.replace(/^### (.+)$/gm, '<h3>$1</h3>');
+
+  // Replace bold text
+  text = text.replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>');
+
+  // Replace bullet points
+  text = text.replace(/^\s*- (.+)$/gm, '<li>$1</li>');
+
+  // Wrap lists in <ul> tags
+  text = text.replace(/(<li>.*<\/li>\n)+/gs, '<ol>\n$&</ol>\n');
+
+  // Replace links
+  text = text.replace(/\[(.+?)\]\((.+?)\)/g, '<a href="$2">$1</a>');
+
+  // Replace newlines with <br> tags
+  text = text.replace(/\n/g, '<br>\n');
+
+  return text;
+};
+
+function AboutDao({ props }:any) {
   const links = [
-    {
-      name: "Profile",
-      icon: FaUser,
-      href: "/profile",
-      color: "text-[#a6a6a6]",
-    },
-    {
-      name: "Contact",
-      icon: FaEnvelope,
-      href: "/contact",
-      color: "text-[#36b0f7]",
-    },
-    {
-      name: "GitHub",
-      icon: FaGithub,
-      href: "https://github.com",
-      color: "text-[#000]",
-    },
-    {
-      name: "LinkedIn",
-      icon: FaLinkedin,
-      href: "https://linkedin.com",
-      color: "text-[#0a66c2]",
-    },
+    { name: "Profile", icon: FaUser, href: "/profile", color: "text-[#a6a6a6]" },
+    { name: "Contact", icon: FaEnvelope, href: "/contact", color: "text-[#36b0f7]" },
+    { name: "GitHub", icon: FaGithub, href: "https://github.com", color: "text-[#000]" },
+    { name: "LinkedIn", icon: FaLinkedin, href: "https://linkedin.com", color: "text-[#0a66c2]" },
     { name: "Website", icon: FaGlobe, href: "/", color: "text-blue-500" },
-    {
-      name: "Governance",
-      icon: FaBalanceScale,
-      href: "/governance",
-      color: "text-purple-600",
-    },
-    {
-      name: "Documentation",
-      icon: FaBook,
-      href: "/docs",
-      color: "text-gray-700",
-    },
-    {
-      name: "Bug Bounty",
-      icon: FaBug,
-      href: "/bug-bounty",
-      color: "text-red-500",
-    },
+    { name: "Governance", icon: FaBalanceScale, href: "/governance", color: "text-purple-600" },
+    { name: "Documentation", icon: FaBook, href: "/docs", color: "text-gray-700" },
+    { name: "Bug Bounty", icon: FaBug, href: "/bug-bounty", color: "text-red-500" },
     { name: "Blog", icon: FaBlog, href: "/blog", color: "text-indigo-500" },
-    {
-      name: "Mirror",
-      icon: FaMediumM,
-      href: "https://mirror.xyz",
-      color: "text-black",
-    },
-    {
-      name: "Twitch",
-      icon: FaTwitch,
-      href: "https://twitch.tv",
-      color: "text-purple-500",
-    },
+    { name: "Mirror", icon: FaMediumM, href: "https://mirror.xyz", color: "text-black" },
+    { name: "Twitch", icon: FaTwitch, href: "https://twitch.tv", color: "text-purple-500" },
   ];
+
+  const text = props === "arbitrum" ? about.arb_description : about.op_description;
+  const formattedText = processText(text);
 
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="bg-gray-50 shadow-md rounded-lg p-6 mb-10">
-        <p className="text-gray-700">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi magni
-          reprehenderit possimus hic, tempora est culpa quibusdam delectus
-          ratione ex, laborum pariatur? Veritatis rerum quae laudantium
-          similique necessitatibus, eius dolorum. Lorem ipsum dolor sit amet
-          consectetur, adipisicing elit. Doloribus, fugit. Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Numquam incidunt perspiciatis
-          quaerat culpa cumque at consequatur mollitia et nostrum.
-        </p>
+      <div dangerouslySetInnerHTML={{ __html: formattedText }} />
       </div>
       <h2 className="text-2xl font-semibold mb-8 text-blue-shade-100">Important Links</h2>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
