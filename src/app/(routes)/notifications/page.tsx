c4d5fca3c3139080dbd489770f7ebcbfd63189f1
Hiralvala <valahiral563@gmail.com>
2024-07-08 10:51:10 +0530

    Add notification page and some of the changes of design regarding follow feature

---
+++ src/app/(routes)/notifications/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NotificationMain from '@/components/Notification/NotificationMain'
+function page() {
+  return (
+    <div>
+      <NotificationMain />
+    </div>
+  );
+}
+
+export default page;
