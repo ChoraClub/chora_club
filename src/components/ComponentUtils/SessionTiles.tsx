f350433e35e3c53183c05be6147239e4026cbd62
isha-mistry <ishamistry.lamprostech@gmail.com>
2024-07-17 11:33:54 +0530

    separated modal of editing session

--- src/components/ComponentUtils/SessionTiles.tsx
+++ src/components/ComponentUtils/SessionTiles.tsx
@@ -27,6 +27,7 @@ import { FaGift } from "react-icons/fa6";
 import style from "./SessionTiles.module.css";
 // const { ethers } = require("ethers");
 import copy from "copy-to-clipboard";
+import UpdateHostedSessionModal from "./UpdateHostedSessionModal";
 
 type Attendee = {
   attendee_address: string;
@@ -727,80 +728,15 @@ SessionTileProps) {
       </div>
 
       {editOpen && selectedTileIndex !== null && (
-        <>
-          <div className="fixed inset-0 flex items-center justify-center z-50  overflow-hidden">
-            <div
-              className="absolute inset-0 backdrop-blur-md"
-              onClick={handleCloseEdit}
-            ></div>
-            <div className="p-7 border z-50 rounded-2xl w-[35vw]  bg-white flex flex-col gap-3">
-              <div className="text-blue-shade-100 flex justify-center items-center text-3xl font-semibold">
-                Edit Session
-              </div>
-              <div className="mt-4">
-                <label className="block mb-2 font-semibold">Title:</label>
-                <input
-                  type="text"
-                  name="title"
-                  placeholder={sessionDetails[selectedTileIndex].title}
-                  className="w-full px-4 py-2 border rounded-lg bg-[#D9D9D945]"
-                  value={formData.title}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="mt-2">
-                <label className="block mb-2 font-semibold">Description:</label>
-                <textarea
-                  name="description"
-                  placeholder={sessionDetails[selectedTileIndex].description}
-                  className="w-full px-4 py-2 border rounded-lg bg-[#D9D9D945]"
-                  onChange={handleChange}
-                >
-                  {formData.description}
-                </textarea>
-              </div>
-              <div className="mt-2">
-                <label className="block mb-2 font-semibold">
-                  Thumbnail Image:
-                </label>
-                <input
-                  type="file"
-                  name="image"
-                  accept="*/image"
-                  className="w-full px-4 py-2 border rounded-lg bg-[#D9D9D945]"
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="mt-4 flex gap-2 justify-end">
-                <button
-                  className="bg-blue-shade-100 rounded-lg py-2 px-4 text-white"
-                  onClick={() => handleSave(sessionDetails[selectedTileIndex])}
-                >
-                  {loading ? (
-                    <div className="flex items-center justify-center top-10">
-                      <Oval
-                        visible={true}
-                        height="20"
-                        width="20"
-                        color="#0500FF"
-                        secondaryColor="#cdccff"
-                        ariaLabel="oval-loading"
-                      />
-                    </div>
-                  ) : (
-                    <>Save</>
-                  )}
-                </button>
-                <button
-                  className="bg-[#D9D9D945] rounded-lg py-2 px-3"
-                  onClick={handleCloseEdit}
-                >
-                  Cancel
-                </button>
-              </div>
-            </div>
-          </div>
-        </>
+        <UpdateHostedSessionModal
+          isOpen={editOpen}
+          onClose={handleCloseEdit}
+          onSave={handleSave}
+          sessionData={sessionDetails[selectedTileIndex]}
+          formData={formData}
+          handleChange={handleChange}
+          loading={loading}
+        />
       )}
     </>
   );
