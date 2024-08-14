85f2b6c4c590d0284a215a1cec2e7069f2e0dd32
purvik6062 <purvikpanchal6336@gmail.com>
2024-08-14 10:49:15 +0530

    zustand implementation

--- src/components/Notification/NotificationMain.tsx
+++ src/components/Notification/NotificationMain.tsx
@@ -16,62 +16,7 @@ import { useSession } from "next-auth/react";
 import { MagnifyingGlass } from "react-loader-spinner";
 import { Session } from "next-auth";
 import toast from "react-hot-toast";
-
-// Custom hook for notifications
-const useNotifications = ({
-  address,
-  session,
-}: {
-  address: string | undefined;
-  session: Session | null;
-}) => {
-  const [notifications, setNotifications] = useState<Notification[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  const fetchNotifications = useCallback(async () => {
-    if (!session) return;
-    setIsLoading(true);
-    try {
-      const myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
-
-      const raw = JSON.stringify({ address });
-
-      const requestOptions: RequestInit = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-      };
-      const response = await fetch("/api/notifications", requestOptions);
-      const result = await response.json();
-      if (Array.isArray(result.data)) {
-        setNotifications(result.data);
-      } else {
-        console.error("Fetched data is not an array:", result);
-      }
-    } catch (error) {
-      console.error("Error fetching notifications:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [address, session]);
-
-  const canFetch = !!address && !!session;
-
-  useEffect(() => {
-    if (canFetch) {
-      fetchNotifications();
-    }
-  }, [fetchNotifications, canFetch]);
-
-  return {
-    notifications,
-    setNotifications,
-    isLoading,
-    fetchNotifications,
-    canFetch,
-  };
-};
+import { useNotificationStudioState } from "@/store/notificationStudioState";
 
 function NotificationMain() {
   const { data: session } = useSession();
@@ -82,15 +27,21 @@ function NotificationMain() {
   const socket = useSocket();
   const {
     notifications,
-    setNotifications,
-    isLoading,
-    fetchNotifications,
+    newNotifications,
+    combinedNotifications,
     canFetch,
-  } = useNotifications({ address, session });
-  const [newNotifications, setNewNotifications] = useState<Notification[]>([]);
+    setNotifications,
+    setNewNotifications,
+    addNotification,
+    updateCombinedNotifications,
+    markAllAsRead,
+    setCanFetch,
+  } = useNotificationStudioState();
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [socketId, setSocketId] = useState<string | null>(null);
   const [buttonText, setButtonText] = useState("Mark all as read");
-  const [markAllReadCalling, setMarkAllReadCalling] = useState<boolean>();
+  const [markAllReadCalling, setMarkAllReadCalling] = useState<boolean>(false);
   const [isPageLoading, setIsPageLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -110,39 +61,71 @@ function NotificationMain() {
   }, [socket]);
 
   useEffect(() => {
-    if (socket && session && socketId && address) {
-      const handleNewNotification = (message: Notification) => {
+    // Set canFetch based on address and session
+    setCanFetch(!!address && !!session);
+  }, [address, session, setCanFetch]);
+
+  const fetchNotifications = useCallback(async () => {
+    if (!canFetch) return;
+    setIsLoading(true);
+    try {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      const raw = JSON.stringify({ address });
+
+      const requestOptions: RequestInit = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+      };
+      const response = await fetch("/api/notifications", requestOptions);
+      const result = await response.json();
+      if (Array.isArray(result.data)) {
+        setNotifications(result.data);
+        updateCombinedNotifications();
+      } else {
+        console.error("Fetched data is not an array:", result);
+      }
+    } catch (error) {
+      console.error("Error fetching notifications:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [address, canFetch, setNotifications, updateCombinedNotifications]);
+
+  useEffect(() => {
+    if (canFetch) {
+      fetchNotifications();
+    }
+  }, [fetchNotifications, canFetch]);
+
+  useEffect(() => {
+    if (socket && address && socketId) {
+      socket.emit("register_host", { hostAddress: address, socketId });
+
+      socket.on("new_notification", (message: Notification) => {
         const notificationData: Notification = {
-          ...message,
+          _id: message?._id,
+          receiver_address: message.receiver_address,
+          content: message.content,
           createdAt: Date.now(),
           read_status: false,
+          notification_name: message.notification_name,
+          notification_type: message.notification_type,
+          notification_title: message.notification_title,
         };
-        setNewNotifications((prev) => [...prev, notificationData]);
-      };
-
-      socket.on("connect", () => {
-        socket.emit("register_host", {
-          hostAddress: address,
-          socketId: socket.id,
-        });
+        addNotification(notificationData);
+        updateCombinedNotifications();
       });
-
-      socket.on("new_notification", handleNewNotification);
-
-      return () => {
-        socket.off("new_notification", handleNewNotification);
-      };
     }
-  }, [socket, address, socketId]);
 
-  const combinedNotifications = React.useMemo(
-    () =>
-      [...notifications, ...newNotifications].sort(
-        (a, b) =>
-          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-      ),
-    [notifications, newNotifications]
-  );
+    return () => {
+      if (socket) {
+        socket.off("new_notification");
+      }
+    };
+  }, [socket, address, socketId, addNotification, updateCombinedNotifications]);
 
   const filteredNotifications = React.useMemo(() => {
     const type = searchParams.get("active");
@@ -158,10 +141,10 @@ function NotificationMain() {
     );
   }, [combinedNotifications, searchParams]);
 
-  const markAllAsRead = async () => {
+  const handleMarkAllAsRead = async () => {
     if (!address || !session) return;
 
-    const hasUnreadNotifications = [...notifications, ...newNotifications].some(
+    const hasUnreadNotifications = combinedNotifications.some(
       (notification) => notification.read_status === false
     );
 
@@ -191,12 +174,8 @@ function NotificationMain() {
         requestOptions
       );
       if (response.ok) {
-        setNotifications((prev) =>
-          prev.map((n) => ({ ...n, read_status: true }))
-        );
-        setNewNotifications((prev) =>
-          prev.map((n) => ({ ...n, read_status: true }))
-        );
+        markAllAsRead();
+        updateCombinedNotifications();
         setButtonText("Marked!");
         setTimeout(() => setButtonText("Mark all as read"), 2000);
         toast.success("All notifications marked as read");
@@ -268,9 +247,9 @@ function NotificationMain() {
     const components = {
       all: NotificationAll,
       sessionBookings: SessionBookings,
-      // recordedSessions: RecordedSessions,
-      // followers: Followers,
-      // attestations: Attestation,
+      recordedSessions: RecordedSessions,
+      followers: Followers,
+      attestations: Attestation,
     };
     const Component =
       components[activeTab as keyof typeof components] || NotificationAll;
@@ -314,15 +293,15 @@ function NotificationMain() {
         <div className="ml-auto pe-16">
           <button
             className="my-4 py-2 px-4 border w-52 border-blue-shade-100 text-blue-shade-100 rounded-xl flex items-center shadow-md hover:bg-blue-shade-100 hover:text-white transition duration-300 ease-in-out font-bold"
-            onClick={markAllAsRead}
+            onClick={handleMarkAllAsRead}
             disabled={markAllReadCalling}
           >
-            <PiEnvelopeOpen className="mr-2" size={20} />
+            <PiEnvelopeOpen className="h-5 w-5 mr-2" />
             {buttonText}
           </button>
         </div>
       </div>
-      <div className="px-16">{renderContent()}</div>
+      <div className="flex flex-col pt-7 pl-10 pr-16">{renderContent()}</div>
     </div>
   );
 }
